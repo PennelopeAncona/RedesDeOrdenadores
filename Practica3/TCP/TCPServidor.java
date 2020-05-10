670f9cf27bd7318414f8f7c20a3b4914f216a63c
PennelopeAncona <43093071+PennelopeAncona@users.noreply.github.com>
2020-05-10 17:36:45 +0200

    Update TCPServidor.java

--- Practica3/TCP/TCPServidor.java
+++ Practica3/TCP/TCPServidor.java
@@ -1,4 +1,4 @@
-package Github;
+
 import java.io.IOException;
 import java.net.*;
 public class TCPServer {
@@ -9,20 +9,33 @@ public class TCPServer {
 	public static void main(String[] args) {
 		ServerSocket listenSocket = null;
 		try {
-			int serverPort = 4444;	// Corresponde al servidor 
-			listenSocket = new ServerSocket(serverPort); // inicializamos el socket con el numero del servidor 
+			// Corresponde al servidor 
+			int serverPort = 4444;	
+			// inicializamos el socket con el numero del servidor 
+			listenSocket = new ServerSocket(serverPort); 
 			while(true) {
-				Socket clientSocket = listenSocket.accept(); // conectamos con el cliente depsues de haber sido aceptados
-				if(clientSocket.isConnected()) {	// comprobamos que el cliente está conectado
-					System.out.println("New login"); // verificamos que lo esté a través de un msm
-					Connection connection = new Connection(clientSocket); // establecemos la conexion Servidor-Cliente
-					connection.run();	// iniciamos la actividad del servidor.
+				
+				// conectamos con el cliente depsues de haber sido aceptados
+				Socket clientSocket = listenSocket.accept(); 
+				
+				// comprobamos que el cliente está conectado
+				if(clientSocket.isConnected()) {	
+					
+					// verificamos que lo esté a través de un msm
+					System.out.println("Nueno inicio de sesion: "); 
+					
+					// establecemos la conexion Servidor-Cliente
+					Connection connection = new Connection(clientSocket); 
+					
+					// iniciamos la actividad del servidor.
+					connection.run();	
 				}
 			}
 		} catch (IOException e) {
-			System.out.println("Listen socket:" + e.getMessage());
+			System.out.println("Socket escuchado:" + e.getMessage());
 		}finally {
 			try {
+				//cerramos el socket
 				listenSocket.close();
 			} catch (IOException e) {
 				e.printStackTrace();
