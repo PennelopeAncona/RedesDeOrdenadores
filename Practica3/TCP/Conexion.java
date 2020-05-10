2efd843e80cf8ca4caa2b2a6649752798b084673
PennelopeAncona <43093071+PennelopeAncona@users.noreply.github.com>
2020-05-10 17:33:08 +0200

    Update Conexion.java

--- Practica3/TCP/Conexion.java
+++ Practica3/TCP/Conexion.java
@@ -6,9 +6,9 @@ import java.net.Socket;
 
 public class Connection extends Thread{
 
-	private DataInputStream in;
-	private DataOutputStream out;
-	private Socket clientSocket;
+	private DataInputStream in; // permite la entrada de datos
+	private DataOutputStream out; //permite la salida de informacion
+	private Socket clientSocket; // cliente socket
 	
 	/**
 	 * Establece la conexión cliente-Servidor
@@ -19,10 +19,14 @@ public class Connection extends Thread{
 	public Connection(Socket clientSocket) {
 		try {
 			this.clientSocket = clientSocket;
+			//guardamos los datos de entrada y salida en sus respectivas variables in y out
+			// del cliente socket
 			in = new DataInputStream(clientSocket.getInputStream());
 			out = new DataOutputStream(clientSocket.getOutputStream());
+			
 		} catch (IOException e) {
-			System.out.println("Connection: " + e.getMessage()); // Error Message
+			// Si la conexion no se produce sacamos el mesaje de error de conexion
+			System.out.println("Conexion: " + e.getMessage()); 
 		}
 	}
 	
@@ -31,18 +35,23 @@ public class Connection extends Thread{
 	 */
 	public void run() {
 		try {
+			// guardamos en data el flujo de informacion de entrada del cliente socket
 			String data = in.readUTF();
+			//imprimimos la informacioin recibida
 			System.out.println("recibido : " + data);
 			out.writeUTF(data.toUpperCase()); // Aqui se regresa en dicho formato.
 		} catch (IOException e) {
-			System.out.println("readline: " + e.getMessage());
+			System.out.println("line: " + e.getMessage());
 		}finally {
 			try {
+				//cerramos el socket y liberamos conexion
 				clientSocket.close();
 			} catch (IOException e) {
+				
 				e.printStackTrace();
 			}
 		}
 		
 	}
 }
+
